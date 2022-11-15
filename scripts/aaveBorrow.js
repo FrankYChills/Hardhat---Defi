5560f2e99783afdc022d85ecdc706a15cf2f9f8f
franky <itskchills11@gmail.com>
2022-11-15 19:35:02 +0530

    deposit to aave contract

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,8 +1,80 @@
-const { getWeth } = require("./getWETH");
+const { ethers, network, getNamedAccounts } = require("hardhat");
+const { networkConfig } = require("../helper-hardhat-config");
+const { getWeth, AMOUNT } = require("./getWETH");
 async function main() {
+  const { deployer } = await getNamedAccounts();
   // AAVE protocol treats everything as ERC20 token
   // convert ETH - WETH(ERC20)
+  let erc20Token;
   await getWeth();
+  //get aaves's lending pool contract
+  console.log("Getting lendingPool contract (Aave)");
+  const lendingPool = await getLendingPool(deployer);
+  console.log(`Lending Pool Address : ${lendingPool.address}`);
+  // deposit to aave
+  const wethContractAddress =
+    networkConfig[network.config.chainId]["wethToken"];
+  //lendingpool contract needs to have access to pull the determined amount(WETH) from our wallet
+
+  //approve  (approving lendingPool contract(aave) to use AMOUNT units from our account)
+  console.log("Approving LendingPool (Aave)");
+  await approveErc20(
+    wethContractAddress,
+    lendingPool.address,
+    AMOUNT,
+    deployer
+  );
+  console.log("Depositing to Aave ..");
+  // as our WETH  contract(Token) has 0.02 WETH in it and we have given permission to lendingPool to use 0.02 WETH it should use that and deposit that
+  await lendingPool.deposit(wethContractAddress, AMOUNT, deployer, 0);
+  console.log(
+    `${(AMOUNT / 1e18).toString()} WETH Got successfully deposited to AAVE`
+  );
+  const FinalB = await erc20Token.balanceOf(deployer);
+  console.log(
+    `Your WETH Contract (Token) now has balace Of : ${(
+      FinalB / 1e18
+    ).toString()} WETH`
+  );
+
+  // lendingpooladdressprovider contract address -> 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+  // lendingpooladdressprovider contract ABI's at -> contracts/interfaces/IlendingPool
+  async function getLendingPool(account) {
+    const lendingPoolAddressProvider = await ethers.getContractAt(
+      "ILendingPoolAddressesProvider",
+      networkConfig[network.config.chainId]["lendingPoolAddressesProvider"],
+      account
+    );
+    // get lendingpool contract address
+    const lendingPoolAddresses =
+      await lendingPoolAddressProvider.getLendingPool();
+    // get lendingpool contract abi
+    // lendingpooladdressprovider contract ABI's at -> contracts/interfaces/IlendingPool
+
+    const lendingPool = await ethers.getContractAt(
+      "ILendingPool",
+      lendingPoolAddresses,
+      account
+    );
+    console.log("Got lendingPool Contract");
+    return lendingPool;
+  }
+  async function approveErc20(
+    erc20address, //ERC20 contract address
+    spenderAddress,
+    amountToSpend,
+    account // our account
+  ) {
+    erc20Token = await ethers.getContractAt(
+      "IERC20", // this is contract interface/WETH contract interface
+      erc20address,
+      account
+    );
+    // we are approving spender with certain amount to use in behalf of us
+    console.log("Approving spender ...");
+    const tx = await erc20Token.approve(spenderAddress, amountToSpend);
+    console.log("Spender Approoved!");
+  }
 }
 main()
   .then(() => process.exit(0))
--- scripts/getWETH.js
+++ scripts/getWETH.js
@@ -1,4 +1,5 @@
-const { getNamedAccounts, ethers } = require("hardhat");
+const { getNamedAccounts, ethers, network } = require("hardhat");
+const { networkConfig } = require("../helper-hardhat-config");
 
 const AMOUNT = ethers.utils.parseEther("0.02");
 async function getWeth() {
@@ -9,13 +10,15 @@ async function getWeth() {
   console.log("Finding WETH Contract ..");
   const iWeth = await ethers.getContractAt(
     "IWeth",
-    "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    networkConfig[network.config.chainId]["wethToken"],
     deployer
   );
-  console.log("Contract Found. Depositing ETH to Contract");
+  console.log(
+    `Contract Found. Depositing ${AMOUNT} Units of ETH to WETH Contract`
+  );
   const tx = await iWeth.deposit({ value: AMOUNT });
   await tx.wait(1);
-  console.log("Funds Deposited.Getting Balance from Contract");
+  console.log("Funds Deposited.Getting Balance from WETH Contract");
   const wethBalance = await iWeth.balanceOf(deployer);
   console.log(
     `The WETH balance of ${deployer} is ${wethBalance.toString()} units or ${(
@@ -24,4 +27,4 @@ async function getWeth() {
   );
 }
 
-module.exports = { getWeth };
+module.exports = { getWeth, AMOUNT };
